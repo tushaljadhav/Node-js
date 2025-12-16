c8645e707a972fdfa4fcecc9e16ae088a06c401c
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-12-16 12:15:58 +0000

    create modules

---
+++ hello-world/math.js
@@ -0,0 +1,15 @@
+// function add(a,b){
+//     return a + b
+// }
+
+// function sub(a,b){
+//     return a - b
+// }
+
+// module.exports= {
+//     addFn : add ,
+//     subFn : sub,
+// }
+
+exports.add = (a,b) => a + b
+exports.sub = (a,b)=> a - b
