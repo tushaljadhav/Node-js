a6999a7f5533ea071a6479f0e7d52bc3e39030ba
Tushal <tushalcollege@gmail.com>
2025-12-18 17:58:19 +0530

    big changes

--- 01_basics/index.js
+++ 01_basics/index.js
@@ -5,6 +5,22 @@ const fs = require("fs")
 const myServer = http.createServer((req,res)=>{
     const log = `${Date.now()}: ${req.url}New Req Received\n`
     fs.appendFile('./01_basics/log.txt',log, (err,data)=>{
+        switch (req.url) {
+            case "/":
+                res.end('Hello From Server')
+                
+                break;
+            case "/about":
+                res.end('This is About Page')
+                break;
+            case "/contact":
+                res.end('This is Contact Page')
+                break;
+        
+            default:
+                break;
+                res.end('404 Page Not Found')
+        }
         
         res.end('Hello From Server again') 
 
