01b4c6ead0c8ae8b1442d14a7cff63041c88bafa
Tushal <tushalcollege@gmail.com>
2025-12-18 18:21:21 +0530

    create new file

---
+++ 02_basics/index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const myServer = http.createServer((req, res) => {
+   console.log(req);
+   res.end("Hello from Node.js Server agian!");
+});
+
+myServer.listen(3000, () => console.log("Server Started on port 3000"));
