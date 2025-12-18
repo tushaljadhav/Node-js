cc789d6881288a9a0758128cab52601f8dc3d4d7
Tushal <tushalcollege@gmail.com>
2025-12-18 19:41:18 +0530

    once agian

--- 02_basics/index.js
+++ 02_basics/index.js
@@ -4,13 +4,14 @@ const url = require('url');
 
 const myServer = http.createServer((req, res) => {
    if(req.url==='/favicon.ico') return res.end(); 
-   const log = `${Date.now()}: ${req.url}New Req Received\n`;
+   const log = `${Date.now()}:${req.method}  ${req.url}New Req Received\n`;
    const myUrl = url.parse(req.url,true);
-   console.log(myUrl);
+   // console.log(myUrl);
    
    fs.appendFile('./02_basics/log.txt',log,(err,data)=>{
       switch(myUrl.pathname){
-         case '/': res.end("Home Page");
+         case '/': 
+         if(req.method==='GET') res.end("Home Page");
          break;
          case '/about': res.end("About Page");
          break;
@@ -22,6 +23,9 @@ const myServer = http.createServer((req, res) => {
          const username = myUrl.query.myname;   
          res.end(`hello , ${username}`);
          break;
+         case '/signup':
+         if(req.method==='GET') res.end("Signup Page");
+         else if(req.method==='POST') res.end("Sucess");
          default: res.end("404 Page Not Found"); 
       }
       
