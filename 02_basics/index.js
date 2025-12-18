95da60b980f2da63d68d80c1b2388ec316ff8da5
Tushal <tushalcollege@gmail.com>
2025-12-18 19:21:20 +0530

    completed

--- 02_basics/index.js
+++ 02_basics/index.js
@@ -1,9 +1,32 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 const myServer = http.createServer((req, res) => {
-   console.log(req);
-   res.end("Hello from Node.js Server agian!");
+   if(req.url==='/favicon.ico') return res.end(); 
+   const log = `${Date.now()}: ${req.url}New Req Received\n`;
+   const myUrl = url.parse(req.url,true);
+   console.log(myUrl);
+   
+   fs.appendFile('./02_basics/log.txt',log,(err,data)=>{
+      switch(myUrl.pathname){
+         case '/': res.end("Home Page");
+         break;
+         case '/about': res.end("About Page");
+         break;
+         case '/search':
+            const search = myUrl.query.search_query;
+            res.end("You searched for "+ search);
+         break;
+         case '/contact':
+         const username = myUrl.query.myname;   
+         res.end(`hello , ${username}`);
+         break;
+         default: res.end("404 Page Not Found"); 
+      }
+      
+   })
 });
 
 
-myServer.listen(3000, () => console.log("Server Started on port 3000"));
+myServer.listen(4000, () => console.log("Server Started on port 4000"));
