bca85a5cf546fce572fd4cb3003c6f2e95c929cb
Tushal <tushalcollege@gmail.com>
2025-12-19 17:35:05 +0530

    using express it make easyer

--- 02_basics/index.js
+++ 02_basics/index.js
@@ -1,36 +1,18 @@
 const http = require('http');
-const fs = require('fs');
-const url = require('url');
+const express = require('express');
 
-const myServer = http.createServer((req, res) => {
-   if(req.url==='/favicon.ico') return res.end(); 
-   const log = `${Date.now()}:${req.method}  ${req.url}New Req Received\n`;
-   const myUrl = url.parse(req.url,true);
-   // console.log(myUrl);
-   
-   fs.appendFile('./02_basics/log.txt',log,(err,data)=>{
-      switch(myUrl.pathname){
-         case '/': 
-         if(req.method==='GET') res.end("Home Page");
-         break;
-         case '/about': res.end("About Page");
-         break;
-         case '/search':
-            const search = myUrl.query.search_query;
-            res.end("You searched for "+ search);
-         break;
-         case '/contact':
-         const username = myUrl.query.myname;   
-         res.end(`hello , ${username}`);
-         break;
-         case '/signup':
-         if(req.method==='GET') res.end("Signup Page");
-         else if(req.method==='POST') res.end("Sucess");
-         default: res.end("404 Page Not Found"); 
-      }
-      
-   })
+ const app = express();
+
+ app.get('/', (req, res) => {
+   return res.send('Home Page');
 });
 
+app.get('/about', (req, res) => {
+   return res.send(`Hello ${req.query.name}`);
+});
 
-myServer.listen(4000, () => console.log("Server Started on port 4000"));
+app.listen(8000, () => console.log('Server Started on port 8000'));
+
+// const myServer = http.createServer(app);
+
+// myServer.listen(4000, () => console.log("Server Started on port 4000"));
